6c3309b99e678370ba56e8d6dea8753ca85926ec
saksh <saksh@10.0.0.76>
2020-05-14 16:27:18 -0500

    tst

---
+++ JenKins_Test/src/test/java/GitHubJenkinsTutorial/JenkinsTutorial/Classjenkins.java
@@ -0,0 +1,13 @@
+package GitHubJenkinsTutorial.JenkinsTutorial;
+
+import org.testng.annotations.Test;
+
+public class Classjenkins {
+
+	@Test
+	public void Method1()
+	{
+		System.out.println("This is commit and push Test Method1");
+	}
+
+}
